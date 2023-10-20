9e0df153224ba0e446e003272d0461299b487edd
Jonathan Harvey <jonathanharvey22@gmail.com>
2023-10-19 17:08:51 -0700

    Refactor: refactored javascript carousel, added some css and some small semantics to html

--- main.js
+++ main.js
@@ -1,169 +1,120 @@
-let carouselFrame = document.querySelector('.carousel-frame')
-let perfumeHeadline = document.querySelector('.perfume-headline')
-let perfumeNotes = document.querySelector('.perfume-notes')
-let perfumeImages = document.querySelector('#img-container')
-let carouselNavPrev = document.querySelector('#carousel-prev')
-let carouselNavNext = document.querySelector('#carousel-next')
-let sliderDots = document.querySelectorAll('.selector-dots')
-let currentPerfume;
-let newPerfume;
-let fadeChange;
-let faderIn;
-let faderOut;
+// TODO: 
 
-const carouselTexts = [["Maltese Shore", "Main notes: Coconut, Sandalwood, Vanilla Musk, and Sea Salt"], ["Perseverance", "Main notes: Iris, Jasmine, Vetiver, and Poppy"],["Fern Gully", "Main notes: Clary Sage, Hedione, and Lemongrass"], ["Obsession", "Main notes: Heliotrope, Monoi, and Vanilla"],];
+// Faker.js via python 3.7+
 
-document.querySelector('#carousel-prev').addEventListener('click', carouselArrows)
-document.querySelector('#carousel-next').addEventListener('click', carouselArrows)
+// Build out a db of perfumes
+// Maybe a fake user profile db also?
+// A db of bottles?
 
-addEventListenerDots(sliderDots);
+// Update semantics, and Javascript use of prototypes, other JS features, like maybe
+// currying or something.
 
-function addEventListenerDots(sliderDotsList){
-  for(let i=0; i < sliderDotsList.length; i++){
-    sliderDotsList[i].addEventListener('click', dotsClicked);
+
+(function () {
+  const carouselFrame = document.querySelector('.carousel-frame');
+  const perfumeImages = document.querySelector('#img-container');
+  const sliderDots = document.querySelectorAll('.selector-dots');
+  const carouselNavPrev = document.querySelector('#carousel-prev');
+  const carouselNavNext = document.querySelector('#carousel-next');
+
+  const perfumeData = [
+    {
+      name: "Maltese Shore",
+      notes: "Main notes: Coconut, Sandalwood, Vanilla Musk, and Sea Salt",
+      imageSrc: "./imgs/carousel1.jpg",
+    },
+    {
+      name: "Perseverance",
+      notes: "Main notes: Iris, Jasmine, Vetiver, and Poppy",
+      imageSrc: "./imgs/carousel2.jpg",
+    },
+    {
+      name: "Fern Gully",
+      notes: "Main notes: Clary Sage, Hedione, and Lemongrass",
+      imageSrc: "./imgs/carousel3.jpg",
+    },
+    {
+      name: "Obsession",
+      notes: "Main notes: Heliotrope, Monoi, and Vanilla",
+      imageSrc: "./imgs/carousel4.jpg",
+    },
+  ];
+
+  let currentPerfumeIndex = 0;
+  let automaticCarouselInterval;
+
+  function updatePerfumeDisplay() {
+    const currentPerfume = perfumeData[currentPerfumeIndex];
+    const perfumeImageElements = perfumeImages.children;
+
+    for (let i = 0; i < perfumeImageElements.length; i++) {
+      if (i === currentPerfumeIndex) {
+        fadeIn(perfumeImageElements[i]);
+      } else {
+        fadeOut(perfumeImageElements[i]);
+      }
+      sliderDots[i].classList.toggle('current', i === currentPerfumeIndex);
+    }
+
+    document.querySelector('.perfume-headline').textContent = currentPerfume.name;
+    document.querySelector('.perfume-notes').textContent = currentPerfume.notes;
   }
-}
 
-function carouselArrows(e){
-  interruptCarouselLoop()
-  if(e.target.classList.contains("fa-solid")){
-    if(e.target.parentNode.id == "carousel-prev"){
-      carouselAnimation(-1)
-      // carouselManager(-1)
-    }
-    else{
-      carouselAnimation(1)
-      // carouselManager(1)
-    }
+  function fadeOut(element) {
+    element.style.opacity = 1;
+    (function fade() {
+      if ((element.style.opacity -= 0.1) < 0) {
+        element.style.display = 'none';
+      } else {
+        requestAnimationFrame(fade);
+      }
+    })();
   }
-  else{
-    if(e.target.id =="carousel-prev"){
-      carouselAnimation(-1)
-      // carouselManager(-1)
-    }
-    else{
-      carouselAnimation(1)
-      // carouselManager(1)
-    }
-  }
-}
 
-// when someone clicks on either the arrow, or the dots, I want to pass in some value
-// to another function that manages the carousel loop, and then from that manager
-// changePerfume()
-
-function carouselManager(moveDirection){
-  if(moveDirection < 0){
-    currentPerfumeCalc()
-    if(currentPerfume == "0"){
-      updateDots(currentPerfume, sliderDots.item(3))
-      changePerfume(3)
-    }
-    else{
-      updateDots(currentPerfume, sliderDots.item(currentPerfume - 1))
-      changePerfume(currentPerfume - 1)
-    }
-    //move to current position - 1
+  function fadeIn(element) {
+    element.style.opacity = 0;
+    element.style.display = 'block';
+    (function fade() {
+      let val = parseFloat(element.style.opacity);
+      if (!((val += 0.1) > 1)) {
+        element.style.opacity = val;
+        requestAnimationFrame(fade);
+      }
+    })();
   }
-  else if(moveDirection > 0){
-    currentPerfumeCalc()
-    //move to current position + 1
-    if(currentPerfume == "3"){
-      updateDots(currentPerfume, sliderDots.item(0))
-      changePerfume(0)
-    }
-    else{
-      updateDots(currentPerfume, sliderDots.item(currentPerfume + 1))
-      changePerfume(currentPerfume + 1)
-    }
-  }
-}
 
-function currentPerfumeCalc(){
-  for(let i = 0; i < sliderDots.length; i++){
-    if(sliderDots[i].classList.contains('current')){
-      currentPerfume = i;
-    }
-  }
-}
-
-function dotsClicked(e){
-  interruptCarouselLoop()
-  if (!e.target.classList.contains('current')){
-    currentPerfumeCalc();
-    updateDots(currentPerfume, e.target);
-    changePerfume(newPerfume)
-  }
-}
-
-function updateDots(currentPerfume, newPerfumeCount){
-  sliderDots.item(currentPerfume).classList.remove('current');
-  newPerfumeCount.classList.add('current')
-  for(let i=0; i < sliderDots.length; i++){
-    if(sliderDots[i].classList.contains('current')){
-      newPerfume = i;
+  function handleDotClick(event) {
+    const dotIndex = Array.from(sliderDots).indexOf(event.target);
+    if (dotIndex >= 0) {
+      currentPerfumeIndex = dotIndex;
+      updatePerfumeDisplay();
+      clearInterval(automaticCarouselInterval);
+      startAutomaticCarousel();
     }
   }
-}
 
-function changePerfume(goToPerfumeNumber){
-  //Here I need to toggle visibility, instead of changing the src
-  //See the above updateDots logic, it's by class, and I can use this to toggle visibility
-  for(let i = 0; i < perfumeImages.children.length; i++){
-    if(i === goToPerfumeNumber){
-      perfumeImages.children[i].hidden = false
+  function handleArrowClick(isNext) {
+    if (isNext) {
+      currentPerfumeIndex = (currentPerfumeIndex + 1) % perfumeData.length;
+    } else {
+      currentPerfumeIndex = (currentPerfumeIndex - 1 + perfumeData.length) % perfumeData.length;
     }
-    else{
-      perfumeImages.children[i].hidden = true
-    }
+    updatePerfumeDisplay();
+    clearInterval(automaticCarouselInterval);
+    startAutomaticCarousel();
   }
-  perfumeHeadline.innerText = carouselTexts[goToPerfumeNumber][0];
-  perfumeNotes.innerText = carouselTexts[goToPerfumeNumber][1];
-}
 
-function carouselAnimation(movementDirection){
-  fadeOut();
-  faderIn = setTimeout(fadeIn, 400);
-  fadeChange = setTimeout(carouselManager, 300, movementDirection)
-}
-function fadeIn(){
-  if(carouselFrame.classList.contains('fadein')){
-    //not sure what to do here yet, need to sort out setTimeout() first
+  function startAutomaticCarousel() {
+    automaticCarouselInterval = setInterval(() => {
+      currentPerfumeIndex = (currentPerfumeIndex + 1) % perfumeData.length;
+      updatePerfumeDisplay();
+    }, 3000); // Change image every 3 seconds
   }
-  else if(carouselFrame.classList.contains('fadeout')){
-    carouselFrame.classList.replace('fadeout', 'fadein')
-  }
-  else{
-    //if it doesn't have either, why is it fading in?
-  }
-}
-function fadeOut(){
-  if(carouselFrame.classList.contains('fadeout')){
-    //once again, don't know yet what to do here
-  }
-  else if(carouselFrame.classList.contains('fadein')){
-    carouselFrame.classList.replace('fadein', 'fadeout')
-  }
-  else{
-    //if it doesn't have either it's a new cycle
-    carouselFrame.classList.add('fadeout')
-  }
-}
-//So calling carouselManager with a parameter of -1 moves the carousel left,
-//and param of +1 moves it right. Using setInterval() to create a timed loop
-//where the carousel moves from right to left, one item every 2 or 3 seconds,
-//and if interrupted, waits for some 10 seconds to begin again.
-  let carouselLoop = null
-  let waitTime = null
-  function timeTesting(){
-    clearInterval(carouselLoop)
-    clearInterval(waitTime)
-    carouselLoop = setInterval(carouselAnimation, 4000, -1)
-  }
-  timeTesting()
 
-  function interruptCarouselLoop(){
-    clearInterval(carouselLoop)
-    clearInterval(waitTime)
-    waitTime = setInterval(timeTesting, 10000)
-  }
+  document.querySelector('#carousel-prev').addEventListener('click', () => handleArrowClick(false));
+  document.querySelector('#carousel-next').addEventListener('click', () => handleArrowClick(true));
+  sliderDots.forEach(dot => dot.addEventListener('click', handleDotClick));
+
+  updatePerfumeDisplay();
+  startAutomaticCarousel();
+})();
